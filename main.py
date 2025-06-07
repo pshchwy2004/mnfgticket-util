b29955e9d31e108bed9cf1da2a6b05ef87e70cf7
BiiPii <creeperguy908@gmail.com>
2025-06-07 09:02:28 -0400

    setup stuff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-class Service:
-    def __init__(self, name, n_price, c_price):
-        self.name = name
-        self.n = n_price
-        self.c = c_price
+import service
 
+
+class main:
+    print("Hello world!")
+    
---
+++ service.py
@@ -0,0 +1,5 @@
+class Service:
+    def __init__(self, name, n_price, c_price):
+        self.name = name
+        self.n = n_price
+        self.c = c_price
