6b7e6d8f2bd81722799ff2fff183c374375fec00
BiiPii <creeperguy908@gmail.com>
2025-06-07 21:45:39 -0400

    idk lol

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import service
 
+reg_manicure = service.Service("Regular Manicure", 22, 20)
+gel_manicure = service.Service("Gel Manicure", 40, 38)
+reg_pedicure = service.Service("Regular Pedicure", 38, 35)
+gel_pedicure = service.Service("Gel Pedicure", 58, 55)
 
-class main:
-    print("Hello world!")
-    
